5f8e5d32a8e07bc1dd285bd24d13a44ea7df2347
Jane Doe <henning.brods@gmail.com>
2021-08-27 23:33:47 +0100

    Update instabot.py

--- instabot.py
+++ instabot.py
@@ -1,1 +1,14 @@
+from instapy import InstaPy
 
+session = InstaPy(username = "USERNAME", password = "PASSWORD")
+session.login()
+
+session.set_relationship_bounds(enabled = False, max_followers = 900) #True
+
+session.set_do_follow(False, percentage=100) #True
+session.like_by_tags(["gamergirl", "girlgamer", "like4likes", "follow4likes", "feminism", "girlpower", "follow4follow"], amount = 6)
+#session.set_dont_like(["nsfw"])
+
+#session.unfollow_users(amount=6, allFollowing=True, sleep_delay=60)
+
+session.end
